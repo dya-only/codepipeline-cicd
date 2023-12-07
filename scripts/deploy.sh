972323caa35f821302dde3f100d6d10432ffdc4f
dya-only <dyacode@proton.me>
2023-12-07 14:31:12 +0900

    feat: new test

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -5,7 +5,6 @@ cd /home/ec2-user/test-app
 # DOCKER_IMAGE_NAME=test-app
 # DOCKER_CONTAINER_NAME=test-app
 
-docker-compose build
 docker-compose up -d
 
 # docker run -d -p 3001:3000 --name ${DOCKER_CONTAINER_NAME} ${DOCKER_IMAGE_NAME}
